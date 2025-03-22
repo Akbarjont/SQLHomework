4a7a062f8dc8c53d86f05f7a527fec0512bd3a2c
Akbarjont <15akbarjon@gmail.com>
2025-03-22 17:26:46 +0500

    Update sql4filteringandordering.sql

--- lesson-4/homework/sql4filteringandordering.sql
+++ lesson-4/homework/sql4filteringandordering.sql
@@ -9,3 +9,19 @@ select * from products where Price > 50 and Price < 100;
 select distinct Category, ProductName from Products;
 select distinct Category, ProductName from Products order by ProductName desc;
 select top 10 * from Products order by Price DESC;
+select COALESCE (FirstName, LastName) as AnyAvailableName from Employees;
+select distinct Category, Price from Products;
+select * from Employees where Age > 30 and Age < 40 or DepartmentName = 'Marketing';
+select * from Employees order by Salary DESC offset 10 rows FETCH next 10 rows only;
+select * from products where Price <= 1000 and StockQuantity > 50 order by StockQuantity asc;
+select * from products where ProductName LIKE '%E%';
+SELECT * from Employees where DepartmentName in ('HR', 'IT', 'Finance');
+select * from Customers order by City asc, PostalCode desc;
+select top 10 * from Sales order by SaleAmount DESC;
+select FirstName + ' ' + LastName as FullName from Employees;
+select distinct Category, ProductName, Price from products where Price > 50;
+select * from Products where Price < 0.1 * (select AVG(price) from Products);
+select * from Employees where Age < 30 and DepartmentName in ('HR', 'IT');
+select * from Employees where Email like '%@gmail.com';
+select * from Employees where Salary > All (select Salary from Employees where DepartmentName = 'Sales')
+Select * from Orders where OrderDate between DATEADD (DAY, -180, getDATE()) and getDATE()
