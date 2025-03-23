ec8da577b919bf828f27c81ebaafdb03a1af99d0
Akbarjont <15akbarjon@gmail.com>
2025-03-23 13:52:05 +0500

    Create SQLServerandSSMS.sql

---
+++ lesson-1/homework/SQLServerandSSMS.sql
@@ -0,0 +1,2 @@
+data is raw information, database is a bulk of raw information, relational database is a database consisted of interrelated tables, and table is combination of raws and columns filled with data.
+  
