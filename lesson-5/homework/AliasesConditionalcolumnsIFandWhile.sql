e1a0e502d6bc5f0bb7f087f4b5e08311ef9d8c04
Akbarjont <15akbarjon@gmail.com>
2025-04-01 20:57:39 +0500

    Update AliasesConditionalcolumnsIFandWhile.sql

--- lesson-5/homework/AliasesConditionalcolumnsIFandWhile.sql
+++ lesson-5/homework/AliasesConditionalcolumnsIFandWhile.sql
@@ -1,2 +1,7 @@
-Select ProductID, ProductName as Name, Price, Category, StockQuantity from Products;
+Select ProductName Name from Products;
+select * from Customers Client
+select ProductName from Products UNION select ProductName from Products_Discounted
+select ProductID, ProductName, Price, Category, StockQuantity from Products  INTERSECT select ProductID, ProductName, Price, Category, StockQuantity from Products_Discounted
+select distinct FirstName, Country from Customers
+
 
