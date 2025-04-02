c17b114f89b3c8c2ea9181b72da79be44b682aa2
Akbarjont <15akbarjon@gmail.com>
2025-04-02 18:57:21 +0500

    Update AliasesConditionalcolumnsIFandWhile.sql

--- lesson-5/homework/AliasesConditionalcolumnsIFandWhile.sql
+++ lesson-5/homework/AliasesConditionalcolumnsIFandWhile.sql
@@ -1,7 +1,43 @@
-Select ProductName Name from Products;
-select * from Customers Client
-select ProductName from Products UNION select ProductName from Products_Discounted
-select ProductID, ProductName, Price, Category, StockQuantity from Products  INTERSECT select ProductID, ProductName, Price, Category, StockQuantity from Products_Discounted
-select distinct FirstName, Country from Customers
+1. Select ProductName Name from Products;
+2. select * from Customers Client;
+3. select ProductName from Products
+  UNION
+  select ProductName from Products_Discounted;
+4. select * from Products
+  INTERSECT
+  select * from Products_Discounted;
+5. select distinct FirstName, Country from Customers;
+6. select *,
+  Case when Price > 1000 then 'High'
+  else 'Low' 
+  end as PriceType
+  from Products;
+7. select *, IIF (StockQuantity > 100,'Yes','No') as Stock_Availability  from Products_Discounted
+8. select ProductName from Products 
+Union 
+select ProductName from OutOfStock
+9. select * from Products
+EXCEPT
+select * from Products_Discounted 
+10. select *, IIF (Price>1000,'Expensive','Affordable') as 'PriceType' from Products
+11. select * from employees
+where age < 25 or Salary > 60000
+12. update employees
+set Salary = Salary*1.1
+where departmentname = 'HR' or EmployeeID = 5
+13. select * from Products
+INTERSECT
+select * from Products_Discounted 
+14. select *,
+case when SaleAmount > 500 then 'Top Tier' 
+	when SaleAmount > 200 and SaleAmount <= 500 then 'Mid Tier' 
+	else 'Low Tier'
+	end
+from sales
+15. select CustomerID from Orders
+EXCEPT
+select CustomerID from Invoices
+16. select customerid, quantity, case when Quantity =1 then '3%' when Quantity >1 and Quantity <=3 then '5%' else '7%' end as Discount from Orders
+
 
 
