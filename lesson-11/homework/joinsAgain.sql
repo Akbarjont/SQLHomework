9a359f3e2c2a0b2ca0b17766b6a9d37e43854489
Akbarjont <15akbarjon@gmail.com>
2025-04-12 15:18:08 +0500

    Update joinsAgain.sql

--- lesson-11/homework/joinsAgain.sql
+++ lesson-11/homework/joinsAgain.sql
@@ -10,3 +10,23 @@
 8.2. select c.FirstName, o.OrderID, o.TotalAmount OrderTotal from Orders o full join customers c on o.CustomerID=c.CustomerID where o.TotalAmount > 500
 
 9. select p.ProductName, s.SaleDate, s.SaleAmount from Products p full join Sales s on p.ProductID=s.ProductID where YEAR(s.saledate)=2022 or s.SaleAmount>400
+
+10.1. select p.ProductID, p.ProductName, coalesce(sum(s.SaleAmount), 0) TotalSalesAmount from Products p left join Sales s on p.ProductID=s.ProductID group by p.ProductID, p.ProductName
+10.2. select p.ProductID, coalesce(s.totalsales,0) as TotalSalesAmount from Products p left join (select ProductID, sum(s.SaleAmount) as TotalSales from Sales s group by ProductID) s on p.ProductID=S.ProductID
+10.3. select p.ProductID, isnull(s.totalsales,0) as TotalSalesAmount from Products p left join (select ProductID, sum(s.SaleAmount) as TotalSales from Sales s group by ProductID) s on p.ProductID=S.ProductID
+
+11.1. select e.Name, d.DepartmentName, e.Salary from Employees e full join Departments d on e.DepartmentID=d.DepartmentID where d.DepartmentName='Human Resources' and e.Salary>50000
+11.2. select e.Name, d.DepartmentName, e.Salary from Employees e join (select * from Departments d where d.DepartmentName='Human Resources') d on e.DepartmentID=d.DepartmentID where e.Salary>50000
+
+12. select p.ProductName, s.SaleDate, p.StockQuantity from Products p join Sales s on p.ProductID=s.ProductID where YEAR(s.SaleDate)=2023 and p.StockQuantity>50
+13. select e.Name, d.DepartmentName, e.HireDate from Employees e join Departments d on e.DepartmentID=d.DepartmentID where d.DepartmentID=1 or YEAR(e.HireDate)>2020
+14. select c.FirstName, o.OrderID, c.Address, o.OrderDate from Customers c join Orders o on c.CustomerID=o.CustomerID where c.Country='USA' and c.Address like ('____ %')
+15. select p.ProductName, p.Category, s.SaleAmount from Products p join Sales s on p.ProductID=s.ProductID join Categories c on p.Category=c.CategoryID where c.CategoryName = 'Electronics' or s.SaleAmount>350
+
+16.1. select c.CategoryName, count(p.StockQuantity) ProductCount from Categories c join products p on c.CategoryID=p.Category group by c.CategoryName
+16.2. select c.CategoryName, p.ProductCount from Categories c join (select p2.Category, count(p2.StockQuantity) ProductCount from products p2 group by p2.Category) p on c.CategoryID=p.Category
+
+17. select c.FirstName, c.City, o.OrderID, o.TotalAmount from Customers c join Orders o on c.CustomerID=o.CustomerID where c.City='Los Angeles' and o.TotalAmount>300
+18. select * from Employees e join Departments d on e.DepartmentID=d.DepartmentID where d.DepartmentName='Finance' or e.Name like ('%[aeiou]%[aeiou]%[aeiou]%[aeiou]%')
+19. select p.ProductName, SUM(s.saleamount) QuantitySold, p.Price from Sales s join Products p on s.ProductID=p.ProductID where p.Price>500 group by p.ProductName having SUM(s.saleamount) > 100 
+20. select * from Employees e join Departments d on e.DepartmentID=d.DepartmentID where e.salary>60000 and (d.DepartmentName='Sales' or d.DepartmentName='Marketing')
